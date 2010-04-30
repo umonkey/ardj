8d1263eb863531e2a4f099798a955ca6a2faac79
justin.forest <none@none>
2010-04-30 13:10:07 +0400

    Better unicode handling in log.log()

--- ardj/log.py
+++ ardj/log.py
@@ -2,5 +2,7 @@ import sys
 import time
 
 def log(message):
+    if type(message) == unicode:
+        message = message.encode('utf-8')
     message = time.strftime('%Y-%m-%d %H:%M:%S ', time.localtime()) + message.strip()
-    print >>sys.stderr, message.encode('utf-8')
+    print >>sys.stderr, message
