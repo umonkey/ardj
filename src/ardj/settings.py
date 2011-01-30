6fc2c9b1ccb9c398109004eca689448044d8b2f0
justin.forest <none@none>
2011-01-30 13:39:48 +0300

    Even better handling of default settings.

--- src/ardj/settings.py
+++ src/ardj/settings.py
@@ -1,19 +1,12 @@
 import yaml
 import os
 
-def load(app, defaults=None, quiet=False):
+def load(app, defaults=None):
+    defaults = defaults or {}
     for filename in ('/etc/ardj.conf', '~/.config/ardj.conf'):
         filename = os.path.expanduser(filename)
         if os.path.exists(filename):
             tmp = yaml.load(open(filename, 'rb'))
             if type(tmp) == dict and tmp.has_key(app):
-                defaults = defaults or {}
                 defaults.update(tmp[app])
-                return defaults
-            elif defaults is not None:
-                return defaults
-            elif quiet:
-                return {}
-            else:
-                raise Exception('%s has no %s block.' % (filename, app))
-    raise Exception('Config file ardj.conf not found.')
+    return defaults
