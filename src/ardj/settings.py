81a3732db99231a166eb8b3e792490ba32108de6
justin.forest <none@none>
2011-03-08 12:07:35 +0300

    Переписана обработка голосовой почты.
    Update issue 59
    Status: Fixed
    - Робот сам добавляет префикс и суффикс, считает ReplayGain, загружает файлы по
      sftp.
    - Упрощён доступ к конфигурации.
    
    ⌚ 81m

--- src/ardj/settings.py
+++ src/ardj/settings.py
@@ -1,6 +1,31 @@
 import yaml
 import os
 
+class wrapper:
+    def __init__(self, data):
+        self.data = data
+
+    def get(self, key, default=None):
+        if self.data is not None:
+            if self.data.has_key(key):
+                return self.data[key]
+        return default
+
+    def getpath(self, key, default=None):
+        value = self.get(key, default)
+        if value:
+            value = os.path.expanduser(value)
+        return value
+
+    def has_key(self, key):
+        return self.data and self.data.has_key(key)
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __repr__(self):
+        return '<ardj.settings.wrapper data=%s>' % self.data
+
 def load(app, defaults=None):
     defaults = defaults or {}
     for filename in ('~/.config/ardj/default.yaml', '/etc/ardj.yaml'):
@@ -12,4 +37,4 @@ def load(app, defaults=None):
                     defaults.update(tmp[app])
                 else:
                     defaults = tmp[app]
-    return defaults
+    return wrapper(defaults)
