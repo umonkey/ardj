defa4d874bbca26e488df89f96299ff139858ca9
justin.forest <none@none>
2011-04-01 02:09:16 +0400

    ardj news: замена для newsmaker.

--- src/ardj/news.py
+++ src/ardj/news.py
@@ -1,55 +1,51 @@
-#!/usr/bin/env python
-# vim: set fileencoding=utf-8:
-
-import mutagen
 import os
 import shutil
-import subprocess
 import sys
-import tempfile
 import traceback
 
+import ardj
 import ardj.settings
+import ardj.tags
+import ardj.util
 
 DEFAULT_URL = 'http://broadcast.echo.msk.ru:9000/content/current.mp3'
 DEFAULT_OUT = '/tmp/ardj-news.ogg'
 
 def fetch_news():
     track_length = 0
-    output_fn = tempfile.mkstemp(suffix='.ogg')[1]
+    output_fn = ardj.util.mktemp(suffix='.ogg')
     cmd = u"gst-launch-0.10 -q souphttpsrc location=\"%s\" ! decodebin ! audioconvert ! vorbisenc quality=0.5 ! oggmux ! filesink location=\"%s\"" % (ardj.settings.get('news/url', DEFAULT_URL), output_fn)
 
-    from ardj import Open
-    ardj = Open()
-
     try:
-        subprocess.Popen(cmd.split(' ')).wait()
-        if os.stat(output_fn).st_size:
-            subprocess.Popen(['vorbisgain', '-f', '-q', output_fn]).wait()
+        ardj.util.run(cmd.split(' '))
+        if os.stat(str(output_fn)).st_size:
+            ardj.util.run(['vorbisgain', '-f', '-q', output_fn])
 
             target_fn = ardj.settings.getpath('news/out', DEFAULT_OUT)
             if os.path.exists(target_fn):
                 os.unlink(target_fn)
-            os.chmod(output_fn, 0664)
-            shutil.move(output_fn, target_fn)
+            os.chmod(str(output_fn), 0664)
+            shutil.move(str(output_fn), target_fn)
 
-            tags = mutagen.File(target_fn)
+            tags = ardj.tags.raw(target_fn)
             if tags:
                 track_length = int(tags.info.length)
     except Exception, e:
         print >>sys.stderr, 'Could not fetch news: %s' % e
         traceback.print_exc(e)
 
-    track_id = int(ardj.settings.get('news/track_id', '0')
+    track_id = int(ardj.settings.get('news/track_id', '0'))
     if track_id:
-        ardj.database.cursor().execute('UPDATE tracks SET length = ? WHERE id = ?', (track_length, track_id, ))
-        ardj.queue_track(track_id)
+        a = ardj.Open()
+        a.database.cursor().execute('UPDATE tracks SET length = ? WHERE id = ?', (track_length, track_id, ))
+        a.queue_track(track_id)
         if ardj.settings.get('queue') == 'yes':
-            ardj.queue_track(track_id)
-        ardj.database.commit()
+            a.queue_track(track_id)
+        a.database.commit()
 
     return track_length != 0
 
 
-if __name__ == '__main__':
+def update(args):
+    """Updates the news from echo.msk.ru"""
     fetch_news()
