505426025a7353f5c90d7b457206a3a1b06b5975
justin.forest <none@none>
2011-04-18 04:45:00 +0400

    Исправлен лога в консоль.
    Не падает, если локаль отлична от UTF-8.

--- src/ardj/log.py
+++ src/ardj/log.py
@@ -24,17 +24,21 @@ class Logger:
         return cls.instance
 
 def debug(text):
-    print text.strip()
+    try: print text.strip().encode('utf-8')
+    except: pass
     Logger.get().log.debug(text)
 
 def info(text):
-    print text.strip()
+    try: print text.strip().encode('utf-8')
+    except: pass
     Logger.get().log.info(text)
 
 def warning(text):
-    print >>sys.stderr, text.strip()
+    try: print >>sys.stderr, text.strip().encode('utf-8')
+    except: pass
     Logger.get().log.warning(text)
 
 def error(text):
-    print >>sys.stderr, text.strip()
+    try: print >>sys.stderr, text.strip().encode('utf-8')
+    except: pass
     Logger.get().log.error(text)
