9f35c2e60a6987deaae59cb681672662b1f7463c
justin.forest <none@none>
2011-04-20 04:48:28 +0400

    Исправлена команда show (jabber).

--- src/ardj/jabber.py
+++ src/ardj/jabber.py
@@ -380,9 +380,9 @@ class ardjbot(MyFileReceivingBot):
         if track is None:
             return u'No such track.'
         result = u'«%s» by %s' % (track['title'], track['artist'])
-        result += u'; id=%u weight=%.2f playcount=%u length=%s vote=%u last_played=%s. ' % (track['id'], track['weight'] or 0, track['count'] or 0, ardj.util.format_duration(track['length'] or 0), ardj.tracks.get_vote(track['id'], message.getFrom().getStripped()), ardj.util.format_duration(track.get('last_played'), age=True))
+        result += u'; id=%u weight=%.2f playcount=%u length=%s vote=%u last_played=%s. ' % (track['id'], track['weight'] or 0, track['count'] or 0, ardj.util.format_duration(int(track.get('length', 0))), ardj.tracks.get_vote(track['id'], message.getFrom().getStripped()), ardj.util.format_duration(track.get('last_played', 0), age=True))
         if track['labels']:
-            result += u'Labels: @' + u', @'.join(track['labels']) + u'. '
+            result += u'Labels: @' + u','.join(track['labels']) + u'. '
         return result.strip()
 
     @botcmd
