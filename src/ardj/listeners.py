c07daf8df5a01cd29a417b6b8217a4777984849c
justin.forest <none@none>
2011-05-08 02:24:06 +0400

    Заголовок для yesterday.csv, поправлен крон.

--- src/ardj/listeners.py
+++ src/ardj/listeners.py
@@ -72,7 +72,7 @@ def dump_statistics():
         str,
         str,
         lambda w: '%.02f' % w,
-    ], 'statistics/listeners/total_csv', [ 'last_played', 'artist', 'title', 'listeners', 'id', 'weight' ])
+    ], 'statistics/listeners/total_csv', [ 'last_played', 'artist', 'title', 'listeners', 'track_id', 'weight' ])
 
     sql = 'SELECT l.ts, t.id, t.artist, t.title, l.listeners FROM tracks t INNER JOIN playlog l ON l.track_id = t.id WHERE l.ts BETWEEN ? AND ? AND weight > 0 ORDER BY l.ts'
     params = get_yesterday_ts()
@@ -82,7 +82,7 @@ def dump_statistics():
         lambda x: unicode(x).encode('utf-8'),
         lambda x: unicode(x).encode('utf-8'),
         str,
-    ], 'statistics/listeners/yesterday_csv')
+    ], 'statistics/listeners/yesterday_csv', [ 'time', 'track_id', 'artist', 'title', 'listeners' ])
 
 
 def run_cli(args):
