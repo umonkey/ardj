46928eb5654aff81128a2e497305fa76f5c6ba50
justin.forest <none@none>
2011-05-08 01:47:22 +0400

    Исправлен вывод статистики прослушиваний.
    - Статистика обновляется раз в сутки, файл listeners.csv содержит данные за
      прошлые сутки (раньше содержал последние 1000 дорожек).
    - Файл total.csv содержит дату последнего проигрывания песни.

--- src/ardj/listeners.py
+++ src/ardj/listeners.py
@@ -13,8 +13,7 @@ import ardj.util
 USAGE = """Usage: ardj listeners command...
 
 Commands:
-  show recent    -- show what's been listened to recently
-  show total     -- show aggregate track counts
+  stats -- update totals.csv and yesterday.csv
 """
 
 
@@ -36,47 +35,58 @@ def get_count():
     return int(m.group(1))
 
 
-def format_data(data, converters, output=None):
-    f = csv.writer(output or sys.stdout)
-    for row in data:
-        row = [converters[x](row[x]) for x in range(len(row))]
-        f.writerow(row)
+def format_data(sql, params, converters, setting):
+    filename = ardj.settings.getpath(setting)
+    if filename:
+        print 'Writing to %s' % filename
+        data = ardj.database.cursor().execute(sql, params).fetchall()
+        f = csv.writer(open(filename, 'w'))
+        for row in data:
+            row = [converters[x](row[x]) for x in range(len(row))]
+            f.writerow(row)
 
 
+def get_yesterday_ts():
+    """Returns timestamps for yesterday and today midights, according to local
+    time zone."""
+    now = int(time.time())
+    diff = time.daylight and time.altzone or time.timezone
+    today = now - (now % 86400) + diff
+    if diff < 0:
+        today += 86400
+    yesterday = today - 86400
+    return yesterday, today
 
-def show_total(cur=None, output=None):
-    """Returns total playcounts."""
-    cur = cur or ardj.database.cursor()
-    cur.execute('SELECT t.artist, t.title, SUM(l.listeners) AS count FROM tracks t INNER JOIN playlog l ON l.track_id = t.id GROUP BY t.artist, t.title ORDER BY artist, title')
-    format_data(cur.fetchall(), [
+
+def dump_statistics():
+    """Saves both kinds of statistics to configured files (total.csv and
+    yesterday.csv by default)."""
+    sql = 'SELECT max(l.ts), t.artist, t.title, SUM(l.listeners) AS count FROM tracks t INNER JOIN playlog l ON l.track_id = t.id GROUP BY t.artist, t.title ORDER BY artist, title'
+    params = []
+    format_data(sql, params, [
+        lambda d: time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(d)),
         lambda x: unicode(x).encode('utf-8'),
         lambda x: unicode(x).encode('utf-8'),
         str,
-    ], output=output)
+    ], 'statistics/listeners/total_csv')
 
-
-def show_recent(limit=1000, cur=None, output=None):
-    """Returns last X player tracks."""
-    cur = cur or ardj.database.cursor()
-    sql = 'SELECT l.ts, t.id, t.artist, t.title, l.listeners FROM tracks t INNER JOIN playlog l ON l.track_id = t.id ORDER BY l.ts DESC LIMIT ' + str(limit)
-    cur.execute(sql)
-    format_data(cur.fetchall(), [
+    sql = 'SELECT l.ts, t.id, t.artist, t.title, l.listeners FROM tracks t INNER JOIN playlog l ON l.track_id = t.id WHERE l.ts BETWEEN ? AND ? ORDER BY l.ts'
+    params = get_yesterday_ts()
+    format_data(sql, params, [
         lambda d: time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(d)),
         str,
         lambda x: unicode(x).encode('utf-8'),
         lambda x: unicode(x).encode('utf-8'),
         str,
-    ], output=output)
+    ], 'statistics/listeners/yesterday_csv')
 
 
 def run_cli(args):
     """Implements the "ardj listeners" command."""
-    if len(args) >= 2 and args[0] == 'show':
-        if args[1] == 'total':
-            fn = show_total
-        elif args[1] == 'recent':
-            fn = show_recent
-        return fn()
+    command = ''.join(args[:1])
+
+    if command == 'stats':
+        return dump_statistics()
 
     print USAGE
     return 1
