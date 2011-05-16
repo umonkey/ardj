94bca93d4093c9aa98582f150355b7e2ce5ae99a
justin.forest <none@none>
2011-05-17 02:14:49 +0400

    Загрузка новостей с Маяка.
    Update issue 170
    Status: Fixed
    http://code.google.com/p/ardj/wiki/HowTo#Загрузка_новостей_извне

--- src/ardj/news.py
+++ src/ardj/news.py
@@ -9,7 +9,26 @@ import ardj.tags
 import ardj.tracks
 import ardj.util
 
-DEFAULT_URL = 'http://broadcast.echo.msk.ru:9000/content/current.mp3'
+
+class MayakNews:
+    PLAYLIST_URL = 'http://www.radiomayak.ru/player_list.html?mode=0'
+    FILE_URL_BASE = 'http://www.radiomayak.ru/a/%u.asf'
+
+    def get_episode_urls(self):
+        page = self.fetch(self.PLAYLIST_URL)
+        if page is None:
+            return []
+        r = re.findall('&aid=(\d+)', page)
+        ids = sorted(list(set(r)), key=lambda x: int(x), reverse=True)
+        return [self.FILE_URL_BASE % int(id) for id in ids]
+
+    def get_last_episode_url(self):
+        urls = self.get_episode_urls()
+        if urls:
+            return urls[0]
+
+    def fetch(self, url):
+        return ardj.util.fetch(url, quiet=True, ret=True)
 
 
 def transcode(src, dst):
@@ -52,14 +71,16 @@ def transcode(src, dst):
     return tags.get('length')
 
 
-def fetch_news():
-    track_id = ardj.settings.get('news/track_id', fail=True)
+def fetch_news(url, track_id):
     track_filename = ardj.tracks.get_track_by_id(track_id)['filepath']
     if not track_filename.endswith('.ogg'):
         raise Exception('Track %u is not OGG/Vorbis.' % track_id)
     track_length = 0
 
-    source_fn = ardj.util.fetch(ardj.settings.get('news/url', DEFAULT_URL))
+    source_fn = ardj.util.fetch(url)
+    if source_fn is None:
+        return False
+
     track_length = transcode(str(source_fn), track_filename)
     if track_length > 0:
         ardj.database.cursor().execute('UPDATE tracks SET length = ?, count = 0 WHERE id = ?', (track_length, track_id, ))
@@ -70,6 +91,22 @@ def fetch_news():
     return track_length != 0
 
 
-def update(args):
+def update_source(source):
+    url = source.get('url')
+    track_id = source.get('track_id')
+
+    if url == 'mayak':
+        url = MayakNews().get_last_episode_url()
+
+    if url and track_id:
+        fetch_news(url, track_id)
+
+
+def update_all_sources():
+    for source in ardj.settings.get('news/sources', []):
+        update_source(source)
+
+
+def run_cli(args):
     """Updates the news from echo.msk.ru"""
-    fetch_news()
+    update_all_sources()
