48a48dce4ffa66641a8ac20c29421dfc885e17f2
justin.forest <none@none>
2011-09-30 04:06:03 +0300

    SQL-код инициализации БД вынесен в текстовый файл.
    --HG--
    branch : dbserver2

--- src/ardj/database.py
+++ src/ardj/database.py
@@ -33,6 +33,20 @@ import ardj.scrobbler
 import ardj.tracks
 import ardj.util
 
+
+def get_init_statements(dbtype):
+    """Returns a list of SQL statements to initialize the database."""
+    paths = []
+    paths.append(os.path.realpath(os.path.join(os.path.dirname(sys.argv[0]), "../share/database")))
+    paths.append("/usr/local/share/ardj/database")
+    for path in paths:
+        filename = os.path.join(path, dbtype + ".sql")
+        if os.path.exists(filename):
+            lines = file(filename, "rb").read().decode("utf-8").strip().split("\n")
+            return [line for line in lines if line.strip() and not line.startswith("--")]
+    return []
+
+
 class database:
     """
     Interface to the database.
@@ -53,43 +67,6 @@ class database:
 
         self.db.create_collation('UNICODE', ardj.util.ucmp)
         self.db.create_function('ULIKE', 2, self.sqlite_ulike)
-
-        cur = self.db.cursor()
-        cur.execute('CREATE TABLE IF NOT EXISTS playlists (id INTEGER PRIMARY KEY, name TEXT, last_played INTEGER)')
-        cur.execute('CREATE TABLE IF NOT EXISTS tracks (id INTEGER PRIMARY KEY, owner TEXT, filename TEXT, artist TEXT, title TEXT, length INTEGER, weight REAL, real_weight REAL, count INTEGER, last_played INTEGER)')
-        cur.execute('CREATE INDEX IF NOT EXISTS idx_tracks_owner ON tracks (owner)')
-        cur.execute('CREATE INDEX IF NOT EXISTS idx_tracks_last ON tracks (last_played)')
-        cur.execute('CREATE INDEX IF NOT EXISTS idx_tracks_count ON tracks (count)')
-        cur.execute('CREATE INDEX IF NOT EXISTS idx_tracks_weight ON tracks (weight)')
-        cur.execute('CREATE INDEX IF NOT EXISTS idx_tracks_real_weight ON tracks (real_weight)')
-        cur.execute('CREATE TABLE IF NOT EXISTS queue (id INTEGER PRIMARY KEY, track_id INTEGER, owner TEXT)')
-        # экстренный плейлист
-        cur.execute('CREATE TABLE IF NOT EXISTS urgent_playlists (labels TEXT, expires INTEGER)')
-        cur.execute('CREATE INDEX IF NOT EXISTS urgent_playlists_expires ON urgent_playlists (expires)')
-        # метки
-        cur.execute('CREATE TABLE IF NOT EXISTS labels (track_id INTEGER NOT NULL, email TEXT NOT NULL, label TEXT NOT NULL)')
-        cur.execute('CREATE INDEX IF NOT EXISTS idx_labels_track_id ON labels (track_id)')
-        cur.execute('CREATE INDEX IF NOT EXISTS idx_labels_email ON labels (email)')
-        cur.execute('CREATE INDEX IF NOT EXISTS idx_labels_label ON labels (label)')
-        # голоса пользователей
-        cur.execute('CREATE TABLE IF NOT EXISTS votes (track_id INTEGER NOT NULL, email TEXT NOT NULL, vote INTEGER, weight REAL, ts INTEGER)')
-        cur.execute('CREATE INDEX IF NOT EXISTS idx_votes_track_id ON votes (track_id)')
-        cur.execute('CREATE INDEX IF NOT EXISTS idx_votes_email ON votes (email)')
-        cur.execute('CREATE INDEX IF NOT EXISTS idx_votes_ts ON votes (ts)')
-        # карма
-        cur.execute('CREATE TABLE IF NOT EXISTS karma (email TEXT, weight REAL)')
-        cur.execute('CREATE INDEX IF NOT EXISTS idx_karma_email ON karma (email)')
-        # лог проигрываний
-        cur.execute('CREATE TABLE IF NOT EXISTS playlog (ts INTEGER NOT NULL, track_id INTEGER NOT NULL, listeners INTEGER NOT NULL, lastfm INTEGER NOT NULL DEFAULT 0)')
-        cur.execute('CREATE INDEX IF NOT EXISTS idx_playlog_ts ON playlog (ts)')
-        cur.execute('CREATE INDEX IF NOT EXISTS idx_playlog_track_id ON playlog (track_id)')
-        # исходящие сообщения
-        cur.execute('CREATE TABLE IF NOT EXISTS jabber_messages (id INTEGER PRIMARY KEY, re TEXT, message TEXT)')
-        # музыка для загрузки
-        cur.execute('CREATE TABLE IF NOT EXISTS download_queue (artist TEXT PRIMARY KEY, owner TEXT)')
-        # View для подсчёта веса дорожек на основании кармы.
-        # weight = max(0.1, 1 + sum(vote * weight))
-        cur.execute('CREATE VIEW IF NOT EXISTS track_weights AS SELECT v.track_id AS track_id, COUNT(*) AS count, MAX(0.1, 1 + SUM(v.vote * k.weight)) AS weight FROM votes v INNER JOIN karma k ON k.email = v.email GROUP BY v.track_id')
 
     def __del__(self):
         self.commit()
@@ -398,3 +375,12 @@ def run_cli(args):
         print USAGE
     else:
         db.commit()
+
+
+def cli_init(args):
+    """Initializes the database."""
+    db = Open()
+    for statement in get_init_statements("sqlite"):
+        db.execute(statement)
+    db.commit()
+    return True
