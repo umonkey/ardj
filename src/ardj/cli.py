17e70b8ab8961c64d1c274e12ccdbad3adbc9170
justin.forest <none@none>
2011-09-30 05:10:45 +0300

    CLI command handling is now easier to understand and maintain.
    --HG--
    branch : dbserver2

--- src/ardj/cli.py
+++ src/ardj/cli.py
@@ -2,7 +2,13 @@
 
 This module contains the command line interface.  It first initializes the log
 file, then, depending on the command line arguments, loads and executes the
-appropriate submodule."""
+appropriate submodule.
+
+Commands are passed to the ardj binary as the first argument.  Command handlers
+are functions prefixed with "cmd_", which have dashes replaced with
+underscores.  For example, command "merge-votes" is handled by the
+cmd_merge_votes function.
+"""
 
 import logging
 import os
@@ -11,73 +17,216 @@ import traceback
 
 import ardj.log
 
-COMMAND_MAP = [ # (command, module, function, description)
-    ('client', 'client', 'run_cli', 'run the client'),
-    ('config', 'settings', 'edit_cli', 'edit settings'),
-    ('console', 'console', 'run_cli', 'jabber-like CLI'),
-    ('db', 'database', 'run_cli', 'database functions'),
-    ('db-init', 'database', 'cli_init', 'initializes the database'),
-    ('events', 'tout', 'run_cli', 'works with the upcoming events'),
-    ('find-new-tracks', 'tracks', 'find_new_tracks', 'fetch new songs from Last.fm'),
-    ('hotline', 'hotline', 'run_cli', 'work with the hotline'),
-    ('icelog', 'icelogger', 'run_cli', 'work with Icecast logs'),
-    ('jabber', 'jabber', 'run_cli', 'run the jabber bot'),
-    ('listeners', 'listeners', 'run_cli', 'summarizes listeners.csv'),
-    ('mail', 'mail', 'run_cli', 'send or receive mail'),
-    ('map-listeners', 'map', 'update_listeners', 'updates the listeners map'),
-    ('merge-votes', 'database', 'merge_votes', 'merge votes according to jabber/aliases'),
-    ('news', 'news', 'run_cli', 'updates news from echo.msk.ru'),
-    ('rg', 'replaygain', 'run_cli', 'scan ReplayGain in files'),
-    ('say', 'speech', 'render_text_cli', 'renders text to voice using festival, then plays it'),
-    ('serve', 'server', 'run_cli', 'start the web server'),
-    ('show-news-from-jamendo', 'jamendo', 'print_new_tracks', 'show new tracks available at jamendo.com'),
-    ('sms', 'sms', 'run_cli', 'send text messages (GSM)'),
-    ('stream', 'stream', 'run_cli', 'handles stream events'),
-    ('tags', 'tags', 'run_cli', 'interact with track metadata'),
-    ('track', 'tracks', 'run_cli', 'works with individual tracks'),
-    ('tsn', 'tsn', 'run', 'prepare and process a new So-So-News episode'),
-    ('twit', 'twitter', 'run_cli', 'interacts with the twitter account'),
-]
 
-def find_command(command_name):
-    for cmd, mod, fun, doc in COMMAND_MAP:
-        if command_name == cmd:
-            try:
-                m = __import__('ardj.' + mod)
-                m = getattr(m, mod)
-                f = getattr(m, fun)
-                return f
-            except Exception, e:
-                logging.error('Command "ardj %s" is broken: %s' % (cmd, e))
+def cmd_client(*args):
+    """run the client"""
+    import client
+    return client.run_cli(args)
 
 
-    if command_name is None:
-        print 'Usage: ardj command\nAvailable commands:'
-    else:
-        print 'Unknown command: "%s", available commands:' % command_name
+def cmd_config(*args):
+    """edit settings"""
+    import settings
+    return settings.edit_cli(args)
 
-    maxlen = max([len(c[0]) for c in COMMAND_MAP])
-    fmt = '  %%-%us   %%s' % maxlen
-    for cmd, mod, fun, doc in sorted(COMMAND_MAP, key=lambda x: x[0]):
+
+def cmd_db(*args):
+    """database functions"""
+    import database
+    return database.run_cli(args)
+
+
+def cmd_db_init(*args):
+    """initializes the database"""
+    import database
+    return database.cli_init(args)
+
+
+def cmd_events(*args):
+    """works with the upcoming events"""
+    import tout
+    return tout.run_cli(args)
+
+
+def cmd_find_new_tracks(*args):
+    """adds new songs from Last.fm"""
+    import tracks
+    return tracks.find_new_tracks(args)
+
+
+def cmd_hotline(*args):
+    """work with the hotline"""
+    import hotline
+    return hotline.run_cli(args)
+
+
+def cmd_icelog(*args):
+    """work with Icecast logs"""
+    import icelogger
+    return icelogger.run_cli(args)
+
+
+def cmd_jabber(*args):
+    """run the jabber bot"""
+    import jabber
+    return jabber.run_cli(args)
+
+
+def cmd_listeners(*args):
+    """summarizes listeners.csv"""
+    import listeners
+    return listeners.run_cli(args)
+
+
+def cmd_mail(*args):
+    """send or receive mail"""
+    import mail
+    return mail.run_cli(args)
+
+
+def cmd_map_listeners(*args):
+    """updates the listeners map"""
+    import map
+    return map.update_listeners(args)
+
+
+def cmd_merge_votes(*args):
+    """merge votes according to jabber/aliases"""
+    import database
+    return database.merge_votes(args)
+
+
+def cmd_news(*args):
+    """updates news from echo.msk.ru"""
+    import news
+    return news.run_cli(args)
+
+
+def cmd_rg(*args):
+    """scan ReplayGain in files"""
+    import replaygain
+    return replaygain.run_cli(args)
+
+
+def cmd_say(*args):
+    """renders text to voice using festival, then plays it"""
+    import speech
+    return speech.render_text_cli(args)
+
+
+def cmd_serve(*args):
+    """start the web server"""
+    import server
+    return server.run_cli(args)
+
+
+def cmd_show_news_from_jamendo(*args):
+    """show new tracks available at jamendo.com"""
+    import jamendo
+    return jamendo.print_new_tracks(args)
+
+
+def cmd_sms(*args):
+    """send text messages (GSM)"""
+    import sms
+    return sms.run_cli(args)
+
+
+def cmd_stream(*args):
+    """handles stream events"""
+    import stream
+    return stream.run_cli(args)
+
+
+def cmd_tags(*args):
+    """interact with track metadata"""
+    import tags
+    return tags.run_cli(args)
+
+
+def cmd_track(*args):
+    """works with individual tracks"""
+    import tracks
+    return tracks.run_cli(args)
+
+
+def cmd_tsn(*args):
+    """prepare and process a new So-So-News episode"""
+    import tsn
+    return tsn.run(args)
+
+
+def cmd_twit(*args):
+    """interacts with the twitter account"""
+    import twitter
+    return twitter.run_cli(args)
+
+
+def cmd_help(*args):
+    """shows this help screen"""
+    commands = []
+
+    for k, v in globals().items():
+        if k.startswith("cmd_"):
+            command = k[4:].replace("_", "-")
+            commands.append((command, getattr(v, "__doc__", "").split("\n")[0].strip()))
+
+    length = max([len(x[0]) for x in commands])
+
+    print "Usage: ardj command\nAvailable commands:"
+    fmt = '  %%-%us   %%s' % length
+    for cmd, doc in sorted(commands):
         print fmt % (cmd, doc)
-    exit(1)
+
+    return False
+
+
+def find_handler(name):
+    """Finds the right command handler.
+
+    The best handler is the one with the exactly matching name.  If that one
+    does not exist, all command handlers starting wigh the specified string are
+    selected.  If more than one exsits, an error message is displayed."""
+
+    commands = globals()
+
+    cmd_name = "cmd_" + name.replace("-", "_")
+    if cmd_name in commands:
+        return commands[cmd_name]
+
+    similar = []
+    for k, v in commands.items():
+        if k.startswith(cmd_name):
+            similar.append(v)
+
+    if len(similar) == 1:
+        return similar[0]
+
+    return None
 
 
 def run(args):
+    """The main command line interface entry point.
+
+    Initializes the log file then looks for the specified command handler and
+    executes it.  If no command was given or no handler could be found,
+    displays a help screen.
+    
+    Returns 1 (i.e., an error) if the handler returned either False or None,
+    otherwise returns zero (clear)."""
     ardj.log.install()
-    if len(args) >= 2 and not args[1].startswith('-'):
-        handler = find_command(args[1])
+
+    if len(args) >= 2:
+        handler = find_handler(args[1])
         if handler is not None:
             try:
-                args = [a.decode('utf-8') for a in args[2:]]
-                exit(handler(args))
+                if handler(*args[1:]):
+                    exit(0)
             except KeyboardInterrupt:
-                print 'Interrupted by user.'
-                exit(1)
+                pass
             except Exception, e:
                 logging.error('ERROR handling command %s: %s' % (args[1], e) + traceback.format_exc(e))
-                exit(1)
-        logging.error('Unknown command: %s' % args[1])
-        exit(1)
+            exit(1)
 
-    find_command(None)
+    cmd_help(*args)
+    exit(1)
