30405817c624a43f75be88e2ea69e467e8061883
justin.forest <none@none>
2011-10-15 17:43:57 +0300

    man ardj
    --HG--
    branch : dbserver2

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ data_files = [
     ('share/ardj/samples', ['share/audio/cubic_undead.mp3', 'share/audio/successful_install.ogg']),
     ('share/ardj/shell-extensions/zsh', ['share/shell-extensions/zsh/_ardj']),
     ('share/doc/ardj/examples', glob('share/doc/examples/*')),
+    ('share/man/man1', ['share/doc/man/ardj.1']),
 ]
 
 classifiers = [
