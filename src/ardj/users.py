d72fd5dad9c6aa116544659eb33801a261664b7f
justin.forest <none@none>
2011-12-04 23:24:18 +0300

    Fixed a bug.
    Update issue 201:
    Fixed some bugs.

--- src/ardj/users.py
+++ src/ardj/users.py
@@ -3,12 +3,12 @@
 import time
 
 from ardj import settings
-from ardj.database import fetch
+from ardj.database import fetch, fetchcol
 
 
 def get_voters():
     """Returns information on voters in tuples (email, count, weight)."""
-    rows = ardj.database.fetch('SELECT v.email, COUNT(*) AS c, k.weight '
+    rows = database.fetch('SELECT v.email, COUNT(*) AS c, k.weight '
         'FROM votes v INNER JOIN karma k ON k.email = v.email '
         'GROUP BY v.email ORDER BY c DESC, k.weight DESC, v.email')
     return rows
@@ -19,7 +19,7 @@ def get_top_recent_voters(count=10, days=14):
     if not count:
         return []
     delta = int(time.time()) - days * 86400
-    emails = ardj.database.fetchcol("SELECT `email`, COUNT(*) AS `c` FROM `votes` "
+    emails = database.fetchcol("SELECT `email`, COUNT(*) AS `c` FROM `votes` "
         "WHERE `ts` >= ? GROUP BY `email` ORDER BY `c` DESC "
         "LIMIT %u" % count, (delta, ))
     return emails
@@ -27,9 +27,9 @@ def get_top_recent_voters(count=10, days=14):
 
 def get_admins(safe=False):
     """Returns jids/emails of admins."""
-    admins = ardj.settings.get("jabber_admins", ardj.settings.get("jabber/access", []))
+    admins = settings.get2("jabber_admins", "jabber/access", [])
     if not safe:
-        count = ardj.settings.get("promote_voters", 0)
-        days = ardj.settings.get("promote_voters_days", 14)
+        count = settings.get("promote_voters", 0)
+        days = settings.get("promote_voters_days", 14)
         admins += get_top_recent_voters(count, days)
     return admins
